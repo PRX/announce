20eb1b410c18b741e8fd1a35334e4f49ff0eb9e3
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-05-06 18:28:22 -0400

    fix method name

--- test/test_configuration.rb
+++ test/test_configuration.rb
@@ -4,13 +4,13 @@ require 'bottler/configuration'
 describe Bottler::Configuration do
 
   it 'will return empty hash without a config file' do
-    Bottler::Configuration.load(pub_sub_file: 'doesntexist.yml')
+    Bottler::Configuration.configure(pub_sub_file: 'doesntexist.yml')
     Bottler.options.wont_be_nil
   end
 
   it 'will return config from a file' do
     file = File.join(File.dirname(__FILE__), 'pub_sub.yml')
-    Bottler::Configuration.load(pub_sub_file: file)
+    Bottler::Configuration.configure(pub_sub_file: file)
     Bottler.options.wont_be_nil
     Bottler.options[:subscribe].wont_be_nil
     Bottler.options[:publish].wont_be_nil
