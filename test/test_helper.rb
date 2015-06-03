73dd6e84919d3afe54b7bfdafb99ff008ebf04cd
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-03 15:03:54 -0400

    use new testing module

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,8 @@ ENV['AWS_ACCOUNT_ID'] = '123456789012'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'announce'
-require 'announce/adapters/test_adapter'
+require 'announce/testing'
+# require 'announce/adapters/test_adapter'
 
 require 'minitest/autorun'
 require 'minitest/spec'
@@ -43,36 +44,6 @@ class TestSubscriber
   end
 end
 
-def last_message
-  Announce::Adapters::TestAdapter::Topic.published_messages.pop
-end
-
-def clear_messages
-  Announce::Adapters::TestAdapter::Topic.published_messages.clear
-end
-
-def last_subscription
-  Announce::Adapters::TestAdapter::Subscriber.subscriptions.pop
-end
-
-def clear_subscriptions
-  Announce::Adapters::TestAdapter::Subscriber.subscriptions.clear
-end
-
-def broker_configured?
-  Announce::Adapters::TestAdapter::BrokerManager.configured?
-end
-
-def reset_broker_config
-  Announce::Adapters::TestAdapter::BrokerManager.reset
-end
-
-def reset_announce
-  Announce.logger = Logger.new('/dev/null')
-  Announce.options[:adapter] = 'test'
-  Announce.options[:queue_name_prefix] = 'test'
-  Announce.options[:app_name] = 'app'
-  clear_messages
-end
+include Announce::Testing
 
 reset_announce
