2595dae37c55ef580351efb773bebcf5d1b5fb4a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2021-11-05 22:06:43 -0400

    Add tests for verifying aws resources to create

--- test/announce/adapters/test_shoryuken_adapter.rb
+++ test/announce/adapters/test_shoryuken_adapter.rb
@@ -19,7 +19,7 @@ describe Announce::Adapters::ShoryukenAdapter do
   let(:shoryuken_adapter_class) { Announce::Adapters::ShoryukenAdapter }
 
   it 'can load an adapter class' do
-    shoryuken_adapter_class.adapter_constantize(:topic).must_equal Announce::Adapters::ShoryukenAdapter::Topic
+    _(shoryuken_adapter_class.adapter_constantize(:topic)).must_equal Announce::Adapters::ShoryukenAdapter::Topic
   end
 
   describe 'BrokerManager' do
@@ -35,19 +35,18 @@ describe Announce::Adapters::ShoryukenAdapter do
     it 'configures for publish and subscribe' do
       manager.stub(:configure_publishing, true) do
         manager.stub(:configure_subscribing, true) do
-          manager.configure.must_equal true
+          _(manager.configure).must_equal true
         end
       end
     end
 
     it 'can configure topics for publishing' do
       Shoryuken::Client.sns.stub(:create_topic, { topic_arn: 'arn' } ) do
-        manager.configure_publishing.must_equal true
+        _(manager.configure_publishing).must_equal true
       end
     end
 
     it 'can configure queues, topics, and subscriptions for receiving' do
-
       sns = Minitest::Mock.new
       sns.expect(:config, {region: 'us-east-1'})
       sns.expect(:create_topic, { topic_arn: 'arn' }, [Hash])
@@ -70,6 +69,20 @@ describe Announce::Adapters::ShoryukenAdapter do
         end
       end
     end
+
+    it 'verifies configuration' do
+      logging_io = StringIO.new
+      Announce.logger = Logger.new(logging_io)
+      verify_manager = manager_class.new(broker_options.merge({verify_only: true}))
+      verify_manager.configure()
+      Announce.logger = Logger.new('/dev/null')
+      logging = logging_io.string
+      _(logging).must_match(/Verify SQS Queue: arn:aws:sqs:.*:test_announce_app_subject_action/)
+      _(logging).must_match(/Verify SNS Topic: arn:aws:sns:.*:test_announce_subject_action/)
+      _(logging).must_match(/Verify Subscription/)
+      _(logging).must_match(/from SNS Topic: arn:aws:sns:.*:test_announce_subject_action/)
+      _(logging).must_match(/to SQS Queue: arn:aws:sqs:.*:test_announce_app_subject_action/)
+    end
   end
 
   describe 'Subscriber' do
@@ -83,8 +96,8 @@ describe Announce::Adapters::ShoryukenAdapter do
 
     it 'implements subscribe' do
       subscriber.subscribe(TestSubscriber, 'subject', ['create', 'delete'], {})
-      Shoryuken.worker_registry.workers('test_announce_app_subject_create').must_equal [TestSubscriber]
-      Shoryuken.worker_registry.workers('test_announce_app_subject_delete').must_equal [TestSubscriber]
+      _(Shoryuken.worker_registry.workers('test_announce_app_subject_create')).must_equal [TestSubscriber]
+      _(Shoryuken.worker_registry.workers('test_announce_app_subject_delete')).must_equal [TestSubscriber]
     end
   end
 
@@ -115,12 +128,12 @@ describe Announce::Adapters::ShoryukenAdapter do
 
     it 'can create the topic in SNS' do
       topic.sns.stub(:create_topic, {topic_arn: 'arn:aws:sns:us-east-1:account_id:test_announce_app_subject_action'}) do
-        sub = topic.create.must_equal 'arn:aws:sns:us-east-1:account_id:test_announce_app_subject_action'
+        _(topic.create).must_equal 'arn:aws:sns:us-east-1:account_id:test_announce_app_subject_action'
       end
     end
 
     it 'returns the ARN' do
-      topic.arn.must_equal 'arn:aws:sns:us-east-1:123456789012:test_announce_subject_action'
+      _(topic.arn).must_equal 'arn:aws:sns:us-east-1:123456789012:test_announce_subject_action'
     end
   end
 end
